7ac1c5d7e38f31749668953b8599957f134e518a
Mariana Faustino <marifaustino@gmail.com>
2024-07-25 18:00:51 -0300

    refactor: limpeza do arquivo main

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
-
-import { Gerente } from "./classes/gerente";
-import { Cliente } from "./classes/cliente";
-import { TipoConta } from "./enums/tipoConta";
-import { Endereco } from "./classes/endereco";
-  
-
-// Testando classes Endereco, Cliente e Gerente e o método de abertura de conta
-const gerente = new Gerente()
-const enderecoClienteTeste = new Endereco()
-const clienteTeste = new Cliente('Mariana', enderecoClienteTeste, '2199999999')
-
-const novaContaTeste = gerente.abrirConta(clienteTeste, TipoConta.ContaCorrente, 100)
-console.log(novaContaTeste)
-
