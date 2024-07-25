1ff0d24112176c9ad6ce51b87d8aa4e105228c6c
Mariana Faustino <marifaustino@gmail.com>
2024-07-25 17:56:21 -0300

    chore: adicionar import do GerenteModule ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GerenteModule } from './gerente/gerente.module';
 
 @Module({
-  imports: [],
+  imports: [GerenteModule],
   controllers: [AppController],
   providers: [AppService],
 })
