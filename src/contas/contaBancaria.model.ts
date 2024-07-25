286f2acaff6c5bddaa70b8fcf9867a9707e207b2
Mariana Faustino <marifaustino@gmail.com>
2024-07-25 20:35:58 -0300

    feat: adicionar gerente na conta do cliente

--- src/contas/contaBancaria.model.ts
+++ src/contas/contaBancaria.model.ts
@@ -1,9 +1,11 @@
 import { TipoConta } from "../enums/tipoConta"
 import { Transacao } from "../classes/transacao"
+import { Gerente } from "src/gerente/gerente.model"
 
 export class ContaBancaria {
     saldo: number
     tipo: TipoConta
+    gerente: Gerente
 
-    constructor(tipo: TipoConta, saldo: number){}
+    constructor(tipo: TipoConta, saldo: number, gerente: Gerente){}
 }
