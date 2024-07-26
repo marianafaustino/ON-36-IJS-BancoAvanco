ee8b74139499081f11e0e525e8f75d75bef05046
Mariana Faustino <marifaustino@gmail.com>
2024-07-26 15:28:12 -0300

    feat: adicionar nome a classe cliente

--- src/cliente/cliente.model.ts
+++ src/cliente/cliente.model.ts
@@ -4,5 +4,6 @@ import { ContaBancaria } from "src/contas/contaBancaria.model"
 export class Cliente {
     id: number
     contas?: ContaBancaria[]
+    nome: string
     constructor(nome: string, id: number){}
 }
