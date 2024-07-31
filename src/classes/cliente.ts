1a2af8efd2d7588d9c8a395796f68cfe6a21cbb3
Mariana Faustino <marifaustino@gmail.com>
2024-07-31 15:19:47 -0300

    ajustes finais realizados na semana 3

---
+++ src/classes/cliente.ts
@@ -0,0 +1,16 @@
+import { ContaBancaria } from "../contas/contaBancaria.model";
+import { Endereco } from "./endereco";
+
+import { Usuario } from "./usuario";
+
+export class Cliente extends Usuario{
+    contaBancaria!: ContaBancaria[]
+    //gerenteDeConta!: Gerente
+
+    constructor(nome: string, endereco: Endereco, telefone: string){
+        super()
+        this.nome = nome
+        this.endereco = endereco
+        this.telefone = telefone
+    }
+}
