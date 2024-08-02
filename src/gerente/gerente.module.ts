82ac5a9beb79afe1a844291374b101e6eca0247b
Mariana Faustino <marifaustino@gmail.com>
2024-08-02 20:59:29 -0300

    refactor: retira do service e controller do gerente responsabilidades que não lhe pertencem

--- src/gerente/gerente.module.ts
+++ src/gerente/gerente.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GerenteService } from './gerente.service';
 import { GerenteController } from './gerente.controller';
-import { ContaBancariaFactory } from 'src/contas/contaBancaria.factory';
+import { ContaBancariaFactory } from 'src/contaBancaria/contaBancaria.factory';
 
 @Module({
   providers: [GerenteService, ContaBancariaFactory],
