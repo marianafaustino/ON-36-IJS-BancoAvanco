d14df8758b4514654f88a59014602f69cd19bc99
Mariana Faustino <marifaustino@gmail.com>
2024-08-02 20:58:57 -0300

    feat: cria construtor para as classes de conta corrente e poupança

--- src/contas/contaCorrente.model.ts
+++
@@ -1,5 +0,0 @@
-import { ContaBancaria } from "../contas/contaBancaria.model";
-
-export class ContaCorrente extends ContaBancaria{
-    private limiteChequeEspecial!: number
-}
--- src/contas/contaPoupanca.model.ts
+++
@@ -1,9 +0,0 @@
-import { ContaBancaria } from "../contas/contaBancaria.model";
-
-export class ContaPoupanca extends ContaBancaria{
-    private rendimentoPoupanca!: number
-    
-    get rendimento(): number {
-        return this.saldo * 0.0001; // 0,01% do saldo
-      }
-}
