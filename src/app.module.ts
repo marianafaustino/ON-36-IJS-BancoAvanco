934db3c8edd3cd3722197599331b16fc8d83bd5b
marianafaustino <marifaustino@gmail.com>
2024-08-16 17:21:19 -0300

    chore: remaneja arquivos entre pastas e ajusta seus caminhos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GerenteModule } from './gerente/gerente.module';
+import { GerenteModule } from './aplicacao/modulos/gerente.module';
 import { ContasService } from './aplicacao/servicos/contaBancaria.service';
 import { ClienteService } from './aplicacao/servicos/cliente.service';
 import { ClienteController } from '../src/aplicacao/controladores/cliente.controller';
 import { ContaBancariaController } from './aplicacao/controladores/contaBancaria.controller';
-import { CepService } from './cep/cep.service';
-import { CepController } from './cep/cep.controller';
-import { CepModule } from './cep/cep.module';
-import { ContaBancariaModule } from './contaBancaria/contaBancaria.module';
+import { CepService } from './infraestrutura/cep/cep.service';
+import { CepController } from './infraestrutura/cep/cep.controller';
+import { CepModule } from './infraestrutura/cep/cep.module';
+import { ContaBancariaModule } from '../src/aplicacao/modulos/contaBancaria.module';
 import { ContaBancariaFactory } from './dominio/fabrica/contaBancaria.factory';
 import { HttpModule } from '@nestjs/axios';
 
